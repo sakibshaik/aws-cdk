b2bd19cbba7af1b5c300cf49a2db4761f85dbacb
sakib.shaik.maniyar <sakib.shaik.maniyar@accenture.com>
2023-01-04 10:59:09 +0000

    create basic quotes endpoint and deploy

--- cdk-quotes-api/lib/cdk-quotes-api-stack.ts
+++ cdk-quotes-api/lib/cdk-quotes-api-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+import { join } from 'path';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkQuotesApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const getQuotes = new Function(this, 'getQuotesLambda', {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(join(__dirname, '../lambdas')),
+      handler: "getQuotes.handler",
+    })
 
-    // The code that defines your stack goes here
+    const api = new RestApi(this, 'quotes-api', {
+      description: "Quotes API",
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQuotesApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    })
+
+    const mainPath = api.root.addResource('quotes');
+    mainPath.addMethod("GET", new LambdaIntegration(getQuotes));
+
+
   }
 }
