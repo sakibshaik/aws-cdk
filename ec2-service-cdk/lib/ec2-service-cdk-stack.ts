39a3654ced252ee60b46f8c7b03190285b2be5ec
sakib.shaik.maniyar <sakib.shaik.maniyar@accenture.com>
2023-01-09 10:09:34 +0000

    ec2: init and generate bash

---
+++ ec2-service-cdk/lib/ec2-service-cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Ec2ServiceCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Ec2ServiceCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
