2fa8230b5a7f32435a7e33eb10be84e7e6253b9f
sakib.shaik.maniyar <sakib.shaik.maniyar@accenture.com>
2023-01-10 09:50:32 +0000

    pipeline: adding a source stage in the pipeline

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -1,11 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Pipeline, Artifact} from "aws-cdk-lib/aws-codepipeline";
+import {GitHubSourceAction} from "aws-cdk-lib/aws-codepipeline-actions";
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+  const pipeline = new Pipeline(this, 'Pipeline', {
+    pipelineName: 'Pipeline',
+    crossAccountKeys: false,
+  })
+
+    const sourceOutput = new Artifact('SourceOutput');
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new GitHubSourceAction({
+          owner: 'sakibshaik',
+          repo: 'aws-cdk',
+          branch: 'main',
+          actionName: 'Pipeline Source',
+          oauthToken: cdk.SecretValue.secretsManager('github-token'),
+          output: sourceOutput,
+        })
+      ]
+
+    })
 
   }
 }
